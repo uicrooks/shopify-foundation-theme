76a14430c7d110bd4f99412535e1b72fe0da6cee
Sergej Samsonenko <contact@sergej.codes>
2021-01-17 11:29:49 +0100

    add forgot password functionality

---
+++ src/vue/components/renderless/template-switch.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  props: {
+    initial: {
+      type: String,
+      required: true,
+      default: null
+    }
+  },
+  data () {
+    return {
+      active: null,
+    }
+  },
+  created () {
+    this.active = this.initial
+  },
+  methods: {
+    show (payload) {
+      this.active = payload
+    }
+  },
+  render () {
+    return this.$scopedSlots.default({
+      active: this.active,
+      show: this.show
+    })
+  }
+}
+</script>
