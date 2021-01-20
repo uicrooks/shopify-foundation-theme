6f688497f05afc8b95a03fc3afa8ee756b256cb7
Sergej Samsonenko <contact@sergej.codes>
2021-01-20 21:07:26 +0100

    add product gallery

---
+++ src/vue/components/renderless/product-gallery.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  props: {
+    featuredImage: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      initialRender: true,
+      currentImage: null
+    }
+  },
+  methods: {
+    switchImage ($el) {
+      this.initialRender = false
+      this.currentImage = {
+        url: $el.target.dataset.url,
+        alt: $el.target.dataset.alt || ''
+      }
+    }
+  },
+  render () {
+    return this.$scopedSlots.default({
+      initialRender: this.initialRender,
+      currentImage: this.currentImage,
+      switchImage: this.switchImage
+    })
+  }
+}
+</script>
